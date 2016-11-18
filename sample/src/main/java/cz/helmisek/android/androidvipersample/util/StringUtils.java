7da6e8a5a0552de9c4321fa9cb828138940b91b3
Jiri Helmich <jiri.helmich@strv.com>
2016-11-18 23:17:01 +0100

    formatting StringUtils.java

--- sample/src/main/java/cz/helmisek/android/androidvipersample/util/StringUtils.java
+++ sample/src/main/java/cz/helmisek/android/androidvipersample/util/StringUtils.java
@@ -7,7 +7,6 @@ import java.util.Locale;
 
 public class StringUtils
 {
-
 	public static String parseTemperature(@NonNull final Double value)
 	{
 		return String.format(Locale.getDefault(), "%.1f", value).replace(",", ".").concat("°C");
